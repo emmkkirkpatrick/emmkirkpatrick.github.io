32610c78b38a1c4655c40d8e181a1445b281b182
Emma Kirkpatrick <106615620+emmkkirkpatrick@users.noreply.github.com>
2023-03-19 23:23:24 -0600

    Update wa9.js

--- wa/wa9.js
+++ wa/wa9.js
@@ -1,6 +1,3 @@
-
-
-
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');         
@@ -11,10 +8,10 @@ function randomValueFromArray(array){
 }
 
 
-let storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+let storyText = 'Last night :insertx: was walking home, but they made a few wrong turns and ended up :inserty:. This is not unsual, as they often get lost. Then the area suddenly began to glow and :insertx: appeared and :insertz:.';
 let insertX = ['Justin', 'Maddie', 'Ben', 'Justin', 'Emma'];
-let insertY = ["the soup kitchen", "Disneyland", "the White House"];
-let insertZ = [ "spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+let insertY = ["at the C4", "on a boat house", "on the Nile river", "in ohio", "at the jersey shore", "in a radioactive lake",];
+let insertZ = ["grew hotdogs for fingers", "shrunk by 75%", "became sentient", "drooled", "ate a pickle"];
 let newStory = storyText;
 
 let xItem = randomValueFromArray(insertX);
@@ -24,8 +21,8 @@ let zItem = randomValueFromArray(insertZ);
 randomize.addEventListener('click', result);
 
 newStory = newStory.replaceAll(':insertx:', xItem);
-newStory = newStory.replace(':inserty:', yItem);
-newStory = newStory.replace(':insertz:', zItem);
+newStory = newStory.replaceAll(':inserty:', yItem);
+newStory = newStory.replaceAll(':insertz:', zItem);
 
 console.log(customName)
 
