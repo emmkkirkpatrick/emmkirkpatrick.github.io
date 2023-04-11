715179eb0b720d589e95c07649b277989384f6d7
Emma Kirkpatrick <106615620+emmkkirkpatrick@users.noreply.github.com>
2023-04-11 10:32:02 -0600

    ica12

--- ica/ica12/main.js
+++ ica/ica12/main.js
@@ -17,6 +17,7 @@ function random(min, max) {
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+
 }
 
   class Ball {
@@ -28,27 +29,81 @@ function randomRGB() {
       this.color = color;
       this.size = size;
 
-    }
+    
   }
-    draw(); {
+    draw() {
       ctx.beginPath();
       ctx.fillStyle = this.color;
       ctx.arc(this.x, this.y, this.size, 0, 2*Math.PI);
+      ctx.fill();
     }
 
-    update(); {
-      this.x = this.x + this.velX;
-      this.y = this.y + this.velY;
-    }
+    update() {
+      if ((this.x + this.size) >= width) {
+         this.velX = -(Math.abs(this.velX));
+      }
 
-class Ball {
+      if ((this.x - this.size) <= 0) {
+         this.velX = Math.abs(this.velX);
+      }
+
+      if ((this.y + this.size) >= height) {
+         this.velY = -(Math.abs(this.velY));
+      }
+
+      if ((this.y - this.size) <= 0) {
+         this.velY = Math.abs(this.velY);
+      }
+
+      this.x += this.velX;
+      this.y += this.velY;
+    }
+    
+    collisionDetect() {
+      for (const ball of balls) {
+         if (!(this === ball)) {
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + ball.size) {
+              ball.color = this.color = randomRGB();
+            }
+         }
+      }
+   }
 
 }
 
+const balls = [];
 
-const testBall = new Ball(100, 100, 2, 2, "red", 10);
+while (balls.length < 25) {
+  const size = random(10,20);
+  const ball = new Ball(
+     // ball position always drawn at least one ball width
+     // away from the edge of the canvas, to avoid drawing errors
+     random(0 + size,width - size),
+     random(0 + size,height - size),
+     random(-7,7),
+     random(-7,7),
+     randomRGB(),
+     size
+  );
 
-testBall.draw();
+ balls.push(ball);
+}
 
+function loop() {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25)';
+  ctx.fillRect(0, 0,  width, height);
 
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
 
+  requestAnimationFrame(loop);
+}
+
+loop();
